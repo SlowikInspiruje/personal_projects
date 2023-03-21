14dc2159afcaf8e046ee3b2590333d69732eb232
SlowikInspiruje <128298704+SlowikInspiruje@users.noreply.github.com>
2023-03-21 10:08:53 +0100

    Delete runtime_snippets/whatsapp_clone_flutter directory

--- runtime_snippets/whatsapp_clone_flutter/android/app/src/main/kotlin/com/example/whatsapp_clone_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.whatsapp_clone_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- runtime_snippets/whatsapp_clone_flutter/lib/theme/app_theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppTheme {
-  static Color green = const Color.fromARGB(255, 66, 193, 82);
-  static Color blue = const Color.fromARGB(255, 39, 110, 237);
-}
